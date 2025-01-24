2e5208ab243065914cc5115c47e07b09648f2e53
Lucas Abreu <lucassantosabreu@gmail.com>
2025-01-24 12:41:32 -0300

    Funcionamento total para o sorteio

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo()
+{
+
+    let amigoInput = document.getElementById('amigo');
+
+    if(amigoInput.value == "")
+    {
+        alert("Campo de nome vazio!");
+    } else {
+        listaAmigos.push(amigoInput.value);
+
+        visualizarLista();
+    
+        amigoInput.value = '';
+    }
+}
+
+function visualizarLista()
+{
+    let listaAmigosHTML = document.getElementById('listaAmigos')
+
+    listaAmigosHTML.innerHTML = "";
+
+    for(let i in listaAmigos)
+    {
+        listaAmigosHTML.innerHTML += "<li>" + listaAmigos[i] + "<li>";
+    }
+
+}
+
+function sortearAmigo()
+{
+    let resultado = document.getElementById('resultado');
+    let i = Math.floor(Math.random() * listaAmigos.length);
+
+    resultado.innerHTML = listaAmigos[i];
+}
